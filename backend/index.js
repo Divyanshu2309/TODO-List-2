bf4ae64f92256e33751e27bed2559be8f3ad68dc
Divyanshu Agarwal <divyanshuagarwal@Divyanshus-MacBook-Air-2.local>
2024-06-29 19:30:21 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
-// index.js
 const express = require('express');
-
+const cors = require('cors'); // Import the cors package
 const connectDB = require('./dbConection');
 const userRoutes = require('./routes/userRoutes');
 
@@ -9,10 +8,12 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Use CORS and allow requests from localhost:3000
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.use(express.json());
-
-
 
 // Define Routes
 app.use('/users', userRoutes);
